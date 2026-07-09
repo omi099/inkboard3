d1db0d88d40c3df1b1a37b67ac43108824599bf4
omi099 <76110622+omi099@users.noreply.github.com>
2026-07-09 20:13:58 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -436,6 +436,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Input;
+using System.Windows.Input.StylusPlugIns;
 using System.Windows.Ink;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
